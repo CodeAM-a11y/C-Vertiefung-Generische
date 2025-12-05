ad7c3c224161c2fcda4e9e5ce9e4094e456eacdb
Akin Mutaf <akin.mutaf@bitlc.onmicrosoft.com>
2025-12-05 09:24:35 +0100

    Quest Aufgabe fertig

--- QuestDictionary/Program.cs
+++ QuestDictionary/Program.cs
@@ -6,15 +6,18 @@
     }
     public class Quest
     {
+        public bool QuestErledigt { get; set; } = false;
         public string name { get; set; }
         public string Beschreibung { get; set; }
-        public List<Item> benötigteItems = new List<Item>();
+        public List<Item> Belohnungen = new List<Item>();
+        public string benötigteItems;
 
-        public Quest(string name, string Beschreibung)
+        public Quest(string name, string Beschreibung, Item belohnung, string benötigteItems)
         {
             this.name = name;
             this.Beschreibung = Beschreibung;
-
+            Belohnungen.Add(belohnung);
+            this.benötigteItems = benötigteItems;
         }
 
     }
@@ -36,11 +39,26 @@
         public int WeisheitsLvl = 0;
         public Hobbit(string name)
         {
+            Item dietrich = new Item("Dietrich", "TürÖffnen");
             this.name = name;
+            Inventar.Add(dietrich);
         }
-        public void AufgabeErledigen()
+        public void AufgabeErledigen(Quest zuErledigen)
         {
+            if (zuErledigen.QuestErledigt == false)
+            {
 
+                if (Inventar.Exists(x=>x.eigenschaft.Equals(zuErledigen.benötigteItems)))
+                {
+                    foreach (Item toRetreive in zuErledigen.Belohnungen)
+                    {
+                        Inventar.Add(toRetreive);
+                        Console.WriteLine($"erhalten: {toRetreive.name} ");
+                    }
+                    zuErledigen.QuestErledigt = true;
+                    Console.WriteLine($"Gratuliere du hast die Quest {zuErledigen.name} geschafft.");
+                }
+            }
         }
     }
     internal class Program
@@ -48,15 +66,14 @@
         static void Main(string[] args)
         {
             QuestSammlung q1 = new QuestSammlung();
-            Quest DieWache = new Quest("Die Wache für Bergil – „Der Dienst am Tor“", "\"Ich bin jetzt ein Mann von Gondor!\"\r\nAufgabe:\r\nPippin verspricht dem jungen Bergil, dessen Vater in der Schlacht ist, Wache am Tor von Minas Tirith zu halten – nicht aus Pflicht, sondern aus Freundschaft und Respekt. Er dient unter Hauptmann Hador und bleibt wachsam, während die Schatten über das Land kriechen.\r\n");
-            Item nahrung = new Item("lebensmittel","nahrung");
             Item Uniform = new Item("Uniform der Wache", "Verkleidung");
             Item LanzeWache = new Item("Lanze der Wache", "Waffe");
-            Quest ersteQuest = new Quest("Vorräte klauen", "Du musst Vorräte klauen");
-            DieWache.benötigteItems.Add(nahrung);
-            DieWache.benötigteItems.Add(Uniform);
-            DieWache.benötigteItems.Add(LanzeWache);
-
+            Quest DieWache = new Quest("Die Wache für Bergil – „Der Dienst am Tor“", "\"Ich bin jetzt ein Mann von Gondor!\"\r\nAufgabe:\r\nPippin verspricht dem jungen Bergil, dessen Vater in der Schlacht ist, Wache am Tor von Minas Tirith zu halten – nicht aus Pflicht, sondern aus Freundschaft und Respekt. Er dient unter Hauptmann Hador und bleibt wachsam, während die Schatten über das Land kriechen.\r\n", LanzeWache,"nahrung");
+            Item nahrung = new Item("Brot der Stadt","nahrung");
+            Quest ersteQuest = new Quest("Vorräte klauen", "Du musst Vorräte klauen", nahrung, "TürÖffnen");
+            Hobbit h1 = new Hobbit("Frodo");
+            h1.AufgabeErledigen(ersteQuest);
+            h1.AufgabeErledigen(DieWache);
         }
     }
 }
